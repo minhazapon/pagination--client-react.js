48f77bca2fe56b6ab53e03f3c804ed14134dfc5b
minhazapon <minhajulabedinapon@gmail.com>
2024-10-01 04:39:57 -0700

    page added

---
+++ src/PageDetails.jsx
@@ -0,0 +1,23 @@
+
+
+const PageDetails = ({page}) => {
+    
+    const { _id, image, name, details, price} = page
+
+    return (
+        <div>
+
+            <div className="max-w-xs p-6 rounded-md shadow-md bg-black text-gray-50">
+            	<img src={image} alt="" className="object-cover object-center w-full rounded-md h-72 bg-gray-500" />
+            	<div className="mt-6 mb-2">
+            		<span className="block text-xs font-medium tracking-widest uppercase text-violet-400">{name} </span>
+            		<h2 className="text-xl font-semibold tracking-wide"> {details} </h2>
+            	</div>
+            	<p className="text-gray-100"> {price} </p>
+            </div>
+            
+        </div>
+    );
+};
+
+export default PageDetails;
--- src/Paginate.jsx
+++ src/Paginate.jsx
@@ -1,9 +1,62 @@
+import { useEffect, useState } from "react";
+import PageDetails from "./PageDetails";
 
 
 
 const Paginate = () => {
+
+
+    const [page, setPage] = useState([])
+
+
+    useEffect( () =>{
+
+      fetch('http://localhost:5000/paginate')
+      .then( res => res.json())
+      .then( data => setPage(data) )
+
+
+    } , [])
+
+
     return (
-        <div>
+        <div className=" mt-10 mb-10 ml-10 mr-10">
+
+            <div>
+
+
+
+
+             
+             <div className=" flex justify-center">
+                <div className=" grid  md:grid-cols-3 gap-5">
+
+                  {
+                    
+                    page.map( page => <PageDetails key={page._id} page={page} ></PageDetails> )
+
+                  }
+
+
+                </div>
+             </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            </div>
             
         </div>
     );
